54020a3f7d12b03f1aaff46aa90e1f7b796ee8c2
Jessica Cooper <jessicagcooper@gmail.com>
2020-05-11 21:58:12 -0500

    Recursive depth first search working.  Added visited set to parameters.

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -32,9 +32,10 @@ class Graph:
         """
         return self.vertices[vertex_id]
 
-"""START OF TRAVERSALS"""
+    
+    """START OF TRAVERSALS"""
 
-    """BREADTH FIRST TRAVERSAL"""
+    """BREADTH FIRST TRAVERSAL"""    
     def bft(self, starting_vertex):
         """
         Print each vertex in breadth-first order
@@ -61,25 +62,69 @@ class Graph:
 
             ##ENQUEUE ITS NEIGHBORS
             for next_vert in self.get_neighbors(v):
-                q.enqueue(next_vert)
+                ##Don't get stuck in the 3 & 5 weird loop in this tree
+                if next_vert not in visited:
+                    q.enqueue(next_vert)
 
+    """DEPTH FIRST TRAVERSAL"""
     def dft(self, starting_vertex):
         """
         Print each vertex in depth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        s = Stack()
+        s.push(starting_vertex)
 
-    def dft_recursive(self, starting_vertex):
+        ##KEEP TRACK OF VISITED NODES
+        visited = set()
+
+        ##REPEAT UNTIL STACK IS EMPTY
+        while s.size() > 0:
+
+            #POP MOST RECENT VERTEX
+            v = s.pop()
+
+            ##IF IT'S NOT VISITED
+            if v not in visited:
+                print(v)
+
+            ##MARK AS VISITED
+            visited.add(v)
+
+            ##PUSH ITS NEIGHBORS
+            for next_vert in self.get_neighbors(v):
+                ##Don't get stuck in the 3 & 5 weird loop in this tree
+                if next_vert not in visited:
+                    s.push(next_vert)
+
+    def dft_recursive(self, starting_vertex, visited=None):
         """
         Print each vertex in depth-first order
         beginning from starting_vertex.
 
         This should be done using recursion.
         """
-        pass  # TODO
+        s = Stack()
+        s.push(starting_vertex)
+        if visited == None:
+            visited = set()
 
-"""BEGINNING OF SEARCHES"""
+
+        if s.size() > 0:
+            v = s.pop()
+            if v not in visited:
+                print(v)
+            visited.add(v)
+            if len(self.vertices) == len(visited):
+                return
+            else:
+                for next_vert in self.get_neighbors(v):
+                    if next_vert not in visited:
+                ##Don't get stuck in the 3 & 5 weird loop in this tree
+                        s.push(next_vert)
+                return self.dft_recursive(next_vert, visited)
+
+    """BEGINNING OF SEARCHES"""
 
     def bfs(self, starting_vertex, destination_vertex):
         """
@@ -149,6 +194,7 @@ if __name__ == '__main__':
         1, 2, 4, 3, 7, 6, 5
         1, 2, 4, 3, 7, 5, 6
     '''
+    print('BFT:')
     graph.bft(1)
 
     '''
@@ -158,19 +204,21 @@ if __name__ == '__main__':
         1, 2, 4, 7, 6, 3, 5
         1, 2, 4, 6, 3, 5, 7
     '''
+    print('DFT:')
     graph.dft(1)
+    print('DFT Recursive:')
     graph.dft_recursive(1)
 
     '''
     Valid BFS path:
         [1, 2, 4, 6]
     '''
-    print(graph.bfs(1, 6))
+    # print(graph.bfs(1, 6))
 
     '''
     Valid DFS paths:
         [1, 2, 4, 6]
         [1, 2, 4, 7, 6]
     '''
-    print(graph.dfs(1, 6))
-    print(graph.dfs_recursive(1, 6))
+    # print(graph.dfs(1, 6))
+    # print(graph.dfs_recursive(1, 6))
